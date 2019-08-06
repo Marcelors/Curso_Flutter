7626d813513df2833f5e946ec15e980e880f7921
Marcelo Rodrigues Silva <squal_marcelo@hotmail.com>
2019-08-06 16:58:23 -0300

    Adicionando appbar

--- buscador_gifs/lib/main.dart
+++ buscador_gifs/lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:buscador_gifs/iu/home_page.dart';
 
-void main() => runApp(MaterialApp(home: HomePage()));
+void main() => runApp(MaterialApp(home: HomePage(), theme: ThemeData(hintColor: Colors.white),));
