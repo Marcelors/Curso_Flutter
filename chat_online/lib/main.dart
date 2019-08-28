738181d214507eaf5e55763a8d468d68a20c8bca
Marcelo Rodrigues Silva <squal_marcelo@hotmail.com>
2019-08-28 09:14:15 -0300

    Finalizando App

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(MyApp());
 
@@ -50,7 +54,7 @@ void _sendMessage({String text, String imgUrl}) {
     "text": text,
     "imgUrl": imgUrl,
     "senderName": googleSignIn.currentUser.displayName,
-    "senderPhoneUrl": googleSignIn.currentUser.photoUrl
+    "senderPhotoUrl": googleSignIn.currentUser.photoUrl
   });
 }
 
@@ -89,14 +93,27 @@ class _ChatScreenState extends State<ChatScreen> {
         body: Column(
           children: <Widget>[
             Expanded(
-              child: ListView(
-                children: <Widget>[
-                  ChatMessage(),
-                  ChatMessage(),
-                  ChatMessage(),
-                ],
-              ),
-            ),
+                child: StreamBuilder(
+                    stream:
+                        Firestore.instance.collection("messages").snapshots(),
+                    builder: (context, snapshot) {
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.none:
+                        case ConnectionState.waiting:
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        default:
+                          return ListView.builder(
+                              reverse: true,
+                              itemCount: snapshot.data.documents.length,
+                              itemBuilder: (context, index) {
+                                List r =
+                                    snapshot.data.documents.reversed.toList();
+                                return ChatMessage(r[index].data);
+                              });
+                      }
+                    })),
             Divider(
               height: 1.0,
             ),
@@ -144,7 +161,16 @@ class _TextComposerState extends State<TextComposer> {
           children: <Widget>[
             Container(
               child:
-                  IconButton(icon: Icon(Icons.photo_camera), onPressed: () {}),
+                  IconButton(icon: Icon(Icons.photo_camera), onPressed: () async {
+                    await _ensureLoggedIn();
+                    File imgFile = await ImagePicker.pickImage(source: ImageSource.camera);
+                    if(imgFile == null) return;
+                    StorageUploadTask task =  FirebaseStorage.instance.ref().child(googleSignIn.currentUser.id.toString() +
+                        DateTime.now().millisecondsSinceEpoch.toString()).putFile(imgFile);
+                    StorageTaskSnapshot taskSnapshot = await task.onComplete;
+                    String url = await taskSnapshot.ref.getDownloadURL();
+                    _sendMessage(imgUrl: url);
+                  }),
             ),
             Expanded(
               child: TextField(
@@ -194,6 +220,10 @@ class _TextComposerState extends State<TextComposer> {
 //        idToken: credentials.idToken, accessToken: credentials.accessToken));
 
 class ChatMessage extends StatelessWidget {
+  final Map<String, dynamic> data;
+
+  ChatMessage(this.data);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -204,18 +234,23 @@ class ChatMessage extends StatelessWidget {
           Container(
             margin: const EdgeInsets.only(right: 16.0),
             child: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://cdn.icon-icons.com/icons2/1674/PNG/512/person_110935.png"),
+              backgroundImage: NetworkImage(data["senderPhotoUrl"]),
             ),
           ),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Text("Daniel", style: Theme.of(context).textTheme.subhead),
+                Text(data["senderName"],
+                    style: Theme.of(context).textTheme.subhead),
                 Container(
                   margin: const EdgeInsets.only(top: 5.0),
-                  child: Text("text"),
+                  child: data["imgUrl"] != null
+                      ? Image.network(
+                          data["imgUrl"],
+                          width: 250.0,
+                        )
+                      : Text(data["text"]),
                 )
               ],
             ),
