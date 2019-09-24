1b993010ad17aec173937c304bc277f3c350fdd5
Marcelo <squal_marcelo@hotmail.com>
2019-09-23 22:56:42 -0300

    criando tela de produto e login

---
+++ loja_virtual/lib/screens/login_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/user_model.dart';
+import 'package:loja_virtual/screens/signup_screen.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+class LoginScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Entrar"),
+          centerTitle: true,
+          actions: <Widget>[
+            FlatButton(
+              child: Text(
+                "CRIA CONTA",
+                style: TextStyle(fontSize: 15.0),
+              ),
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => SignUpScreen()));
+              },
+            )
+          ],
+        ),
+        body: ScopedModelDescendant<UserModel>(
+          builder: (context, child, model) {
+            if(model.isLoading)
+              return Center(child: CircularProgressIndicator());
+
+            return Form(
+              key: _formKey,
+              child: ListView(
+                padding: EdgeInsets.all(16.0),
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(hintText: "E-Mail"),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (text) {
+                      if (text.isEmpty || !text.contains("@"))
+                        return "E-Mail inválido";
+                      return null;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  TextFormField(
+                    decoration: InputDecoration(hintText: "Senha"),
+                    obscureText: true,
+                    validator: (text) {
+                      if (text.isEmpty || text.length < 6)
+                        return "Senha inválida";
+                      return null;
+                    },
+                  ),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Esqueci minha senha",
+                        textAlign: TextAlign.right,
+                      ),
+                      padding: EdgeInsets.zero,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  SizedBox(
+                    height: 44.0,
+                    child: RaisedButton(
+                      child: Text(
+                        "Entrar",
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                      textColor: Colors.white,
+                      color: Theme.of(context).primaryColor,
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+
+                        }
+                        model.signIn();
+                      },
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        ));
+  }
+}
---
+++ loja_virtual/lib/screens/signup_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Criar Conta"),
+        centerTitle: true,
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(hintText: "Nome Completo"),
+              validator: (text) {
+                if (text.isEmpty)
+                  return "Nome inválido";
+                return null;
+              },
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(hintText: "E-Mail"),
+              keyboardType: TextInputType.emailAddress,
+              validator: (text) {
+                if (text.isEmpty || !text.contains("@"))
+                  return "E-Mail inválido";
+                return null;
+              },
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(hintText: "Senha"),
+              obscureText: true,
+              validator: (text){
+                if (text.isEmpty || text.length < 6)
+                  return "Senha inválida";
+                return null;
+              },
+            ),
+            SizedBox(height: 16.0),
+            TextFormField(
+              decoration: InputDecoration(hintText: "Endereço"),
+              validator: (text){
+                if (text.isEmpty)
+                  return "Endereço inválida";
+                return null;
+              },
+            ),
+            SizedBox(height: 16.0),
+            SizedBox(
+              height: 44.0,
+              child: RaisedButton(
+                child: Text(
+                  "Criar Conta",
+                  style: TextStyle(fontSize: 18.0),
+                ),
+                textColor: Colors.white,
+                color: Theme.of(context).primaryColor,
+                onPressed: () {
+                  if(_formKey.currentState.validate()){
+
+                  }
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
