7aabb7b30edf2f34e4164332fb2445979610f8f2
Marcelo Rodrigues Silva <squal_marcelo@hotmail.com>
2020-01-21 13:42:27 -0300

    criando tela de clientes

---
+++ gerenteloja/lib/blocs/user_bloc.dart
@@ -0,0 +1,74 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:rxdart/rxdart.dart';
+
+class UserBloc extends BlocBase {
+  final _usersController = BehaviorSubject<List>();
+
+  Map<String, Map<String, dynamic>> _users = {};
+
+  Firestore _firestore = Firestore.instance;
+
+  Stream<List> get outUsers => _usersController.stream;
+
+  UserBloc() {
+    _addUserListener();
+  }
+
+  void _addUserListener() {
+    _firestore.collection("users").snapshots().listen((snapshot){
+      snapshot.documentChanges.forEach((change){
+        String uid = change.document.documentID;
+
+        switch(change.type){
+
+          case DocumentChangeType.added:
+            _users[uid] = change.document.data;
+            _subscribeToOrders(uid);
+            break;
+          case DocumentChangeType.modified:
+            _users[uid].addAll(change.document.data);
+            _usersController.add(_users.values.toList());
+            break;
+          case DocumentChangeType.removed:
+            _users.remove(uid);
+            _unsubscribeToOrders(uid);
+            _usersController.add(_users.values.toList());
+            break;
+        }
+      });
+    });
+  }
+
+  void _subscribeToOrders(String uid) {
+   _users[uid]["subscribe"] =  _firestore.collection("users").document(uid).collection("orders").snapshots().listen((orders) async{
+      int numOrders = orders.documents.length;
+
+      double money = 0.0;
+
+      for(DocumentSnapshot d in orders.documents){
+        DocumentSnapshot order = await _firestore.collection("orders").document(d.documentID).get();
+
+        if(order.data == null)
+          continue;
+
+        money += order.data["totalPrice"];
+
+        _users[uid].addAll(
+          {"money": money, "orders": numOrders}
+        );
+
+        _usersController.add(_users.values.toList());
+      }
+    });
+  }
+
+  void _unsubscribeToOrders(String uid){
+    _users[uid]["subscribe"].cancel();
+  }
+
+  @override
+  void dispose() {
+    _usersController.close();
+  }
+}
--- gerenteloja/lib/screens/login_screen.dart
+++ gerenteloja/lib/screens/login_screen.dart
@@ -10,9 +10,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   final _loginBloc = LoginBloc();
-
 
   @override
   void dispose() {
@@ -24,18 +22,19 @@ class _LoginScreenState extends State<LoginScreen> {
   void initState() {
     super.initState();
 
-    _loginBloc.outState.listen((state){
-      switch(state){
+    _loginBloc.outState.listen((state) {
+      switch (state) {
         case LoginState.SUCCESS:
           Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context)=> HomeScreen())
-          );
+              MaterialPageRoute(builder: (context) => HomeScreen()));
           break;
         case LoginState.FAIL:
-          showDialog(context: context, builder: (context)=> AlertDialog(
-            title: Text("Erro"),
-            content: Text("Você não possui os privilégios necessàrio"),
-          ));
+          showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                    title: Text("Erro"),
+                    content: Text("Você não possui os privilégios necessàrio"),
+                  ));
           break;
         case LoginState.LOADING:
         case LoginState.IDLE:
@@ -48,71 +47,70 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
         backgroundColor: Colors.grey[700],
         body: StreamBuilder<LoginState>(
-          stream: _loginBloc.outState,
-          initialData: LoginState.LOADING,
+            stream: _loginBloc.outState,
+            initialData: LoginState.LOADING,
             // ignore: missing_return
             builder: (context, snapshot) {
-              // ignore: missing_return
-              switch(snapshot.data){
-                case LoginState.LOADING:
-                  return Center(child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation(Colors.pinkAccent),),);
-                case LoginState.FAIL:
-                case LoginState.SUCCESS:
-                case LoginState.IDLE:
-                  return Stack(
-                    alignment: Alignment.center,
-                    children: <Widget>[
-                      Container(),
-                      SingleChildScrollView(
-                          child: Container(
-                            margin: EdgeInsets.all(16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: <Widget>[
-                                Icon(
-                                  Icons.store_mall_directory,
-                                  color: Colors.pinkAccent,
-                                  size: 160,
-                                ),
-                                InputField(
-                                  icon: Icons.person_outline,
-                                  hint: "Usuário",
-                                  obscure: false,
-                                  stream: _loginBloc.outEmail,
-                                  onChanged: _loginBloc.changeEmail,
-                                ),
-                                InputField(
-                                  icon: Icons.lock_outline,
-                                  hint: "Senha",
-                                  obscure: true,
-                                  stream: _loginBloc.outPassword,
-                                  onChanged: _loginBloc.changePassword,
-                                ),
-                                SizedBox(height: 32,),
-                                StreamBuilder<bool>(
-                                    stream: _loginBloc.outSubmitValid,
-                                    builder: (context, snapshot) {
-                                      return SizedBox(
-                                        height: 50,
-                                        child: RaisedButton(
-                                          color: Colors.pinkAccent,
-                                          child: Text("Entrar"),
-                                          onPressed: snapshot.hasData ? _loginBloc.submit : null,
-                                          textColor: Colors.white,
-                                          disabledColor: Colors.pinkAccent.withAlpha(140),
-                                        ),
-                                      );
-                                    }
-                                )
-                              ],
-                            ),
-                          )
-                      ),
-                    ],
-                  );
+              if (snapshot.data == LoginState.LOADING) {
+                return Center(
+                    child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation(Colors.pinkAccent)));
               }
-            }
-        ));
+
+              return Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Container(),
+                  SingleChildScrollView(
+                      child: Container(
+                    margin: EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(
+                          Icons.store_mall_directory,
+                          color: Colors.pinkAccent,
+                          size: 160,
+                        ),
+                        InputField(
+                          icon: Icons.person_outline,
+                          hint: "Usuário",
+                          obscure: false,
+                          stream: _loginBloc.outEmail,
+                          onChanged: _loginBloc.changeEmail,
+                        ),
+                        InputField(
+                          icon: Icons.lock_outline,
+                          hint: "Senha",
+                          obscure: true,
+                          stream: _loginBloc.outPassword,
+                          onChanged: _loginBloc.changePassword,
+                        ),
+                        SizedBox(
+                          height: 32,
+                        ),
+                        StreamBuilder<bool>(
+                            stream: _loginBloc.outSubmitValid,
+                            builder: (context, snapshot) {
+                              return SizedBox(
+                                height: 50,
+                                child: RaisedButton(
+                                  color: Colors.pinkAccent,
+                                  child: Text("Entrar"),
+                                  onPressed: snapshot.hasData
+                                      ? _loginBloc.submit
+                                      : null,
+                                  textColor: Colors.white,
+                                  disabledColor:
+                                      Colors.pinkAccent.withAlpha(140),
+                                ),
+                              );
+                            })
+                      ],
+                    ),
+                  )),
+                ],
+              );
+            }));
   }
 }
