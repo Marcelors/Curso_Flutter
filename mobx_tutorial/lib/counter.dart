24a7a3b34b02af628c75110d932ee05b49d15131
Marcelo Rodrigues Silva <squal_marcelo@hotmail.com>
2020-04-13 13:30:43 -0300

    criando exemplo com mobx

---
+++ mobx_tutorial/lib/counter.dart
@@ -0,0 +1,31 @@
+import 'package:mobx/mobx.dart';
+
+part 'counter.g.dart';
+
+
+class Counter = _Counter with _$Counter;
+
+ abstract class _Counter with Store{
+//
+//  Counter(){
+//    increment = Action(_increment);
+//  }
+//
+//  Observable _count = Observable(0);
+//
+//  int get count => _count.value;
+//
+//  Action increment;
+//
+//  void _increment(){
+//    _count.value++;
+//  }
+
+  @observable
+  int count = 0;
+
+  @action
+  void increment(){
+    count++;
+  }
+}
